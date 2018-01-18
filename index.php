1b78e4751da92467cc5b122224da5d697eb59f61
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2018-01-18 15:34:35 +0100

    added

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  <?php echo 'App Version: 13.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
- <?php echo ' Joakim modoified: '; ?>
+ <?php echo '\n\n Joakim modified: '; ?>
  </body>
 </html>
